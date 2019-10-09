9ede574ae4241fad6a475a7ae29a6f27413b4982
Mayank Kapur <mayankkapur556@gmail.com>
2019-10-09 17:23:03 +0530

    ...

---
+++ models/user.js
@@ -0,0 +1,9 @@
+var mongoose=require("mongoose");
+const UserSchema={
+  name:{type:String,require:true},  
+   college:{type:String,require:true},
+   phone:{type:Number,require:true}, 
+   email:{type:String,require:true}, 
+   
+}
+module.exports=mongoose.model('User',UserSchema);
